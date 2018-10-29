4c791eb286260ba8db3d721bed52439b198b48a0
EDU <0narara0@gmail.com>
2018-10-29 15:40:07 +0900

    second

--- app/src/main/java/com/cmy/www/firstactivity/SecondActivity.java
+++ app/src/main/java/com/cmy/www/firstactivity/SecondActivity.java
@@ -1,13 +1,22 @@
 package com.cmy.www.firstactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/cmy/www/firstactivity/ThirdActivity.java
+++ app/src/main/java/com/cmy/www/firstactivity/ThirdActivity.java
@@ -1,13 +1,20 @@
 package com.cmy.www.firstactivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class ThirdActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
